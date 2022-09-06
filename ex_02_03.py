930ae400159e6d2eba89eb7a61b1a7c5bc8a5e17
Justin Charette <justin.j.charette@gmail.com>
2022-09-06 14:41:23 +0200

    first commit

---
+++ ex_02_03.py
@@ -0,0 +1,6 @@
+# Write a program to calculate pay with inputs for 
+# number of hours and rate
+hours = input("Enter hours: ")
+rate = input("Enter rate: ")
+pay = float(hours) * float(rate) 
+print("Pay:", pay)
